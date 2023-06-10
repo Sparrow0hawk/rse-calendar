d6e96b09d10b0a6a9a3169b812a19ceba7766866
Sparrow0hawk <alexjcoleman@gmail.com>
2023-06-10 21:03:46 +0100

    move python scripts into sub folder

--- _scripts/generate_posts/generate_posts.py
+++ _scripts/generate_posts/generate_posts.py
@@ -8,21 +8,20 @@ import logging
 LOG_LEVEL = os.environ.get("LOGLEVEL")
 logging.basicConfig(level=LOG_LEVEL)
 
-def main(override=False):
 
+def main(override=False):
     with open("_data/main.yaml", "r") as open_file:
         try:
-
             data = yaml.safe_load(open_file)
 
         except yaml.YAMLError as err:
             print(err)
 
-    data = data['events']
+    data = data["events"]
 
     env = Environment(
-        loader= FileSystemLoader(searchpath="./_scripts"),
-        undefined=StrictUndefined
+        loader=FileSystemLoader(searchpath="./_scripts/generate_posts"),
+        undefined=StrictUndefined,
     )
 
     template = env.get_template("post_template.md.j2")
@@ -30,30 +29,29 @@ def main(override=False):
     today = datetime.datetime.today()
 
     for event in data:
+        if event["begin"] >= today:
+            filename = (
+                str(event["begin"].strftime("%Y-%m-%d"))
+                + "-"
+                + event["summary"].strip().replace(" ", "_")
+            )
 
-        if event['begin'] >= today:
-
-            filename = str(event['begin'].strftime("%Y-%m-%d")) + "-" \
-                + event['summary'].strip().replace(" ","_")
-
-            output_path = os.path.join("_posts",filename + ".md")
+            output_path = os.path.join("_posts", filename + ".md")
 
             if (os.path.exists(output_path)) & (~override):
                 logging.info(f"Skipping rendering {output_path}. It already exists.")
             else:
-
                 rendered = template.render(event)
 
                 with open(output_path, "w") as output_file:
                     output_file.write(rendered)
 
+    return
 
-    return 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     parser = argparse.ArgumentParser("RSE Events page post generator")
-    parser.add_argument("--override",action = "store_true")
+    parser.add_argument("--override", action="store_true")
     args = parser.parse_args()
 
     main(override=args.override)
