9b365bd3b383f14c65fa77a6e2df755fb1f1dfdb
Alex Coleman <alexjcoleman@gmail.com>
2023-08-02 19:22:18 +0000

    turn off date parsing with PyYAML
    approach taken from https://stackoverflow.com/a/37958106/10651182

--- _scripts/generate_posts/generate_posts.py
+++ _scripts/generate_posts/generate_posts.py
@@ -10,9 +10,12 @@ logging.basicConfig(level=LOG_LEVEL)
 
 
 def main(override=False):
+
+    NoDatesSafeLoader.remove_implicit_resolver('tag:yaml.org,2002:timestamp')
+
     with open("_data/main.yaml", "r") as open_file:
         try:
-            data = yaml.safe_load(open_file)
+            data = yaml.load(open_file, Loader = NoDatesSafeLoader)
 
         except yaml.YAMLError as err:
             print(err)
@@ -51,6 +54,30 @@ def main(override=False):
                     output_file.write(rendered)
 
     return
+
+class NoDatesSafeLoader(yaml.SafeLoader):
+    """
+    Copied under CC-BY-SA 3.0
+    https://stackoverflow.com/a/37958106/10651182
+    """
+    @classmethod
+    def remove_implicit_resolver(cls, tag_to_remove):
+        """
+        Remove implicit resolvers for a particular tag
+
+        Takes care not to modify resolvers in super classes.
+
+        We want to load datetimes as strings, not dates, because we
+        go on to serialise as json which doesn't have the advanced types
+        of yaml, and leads to incompatibilities down the track.
+        """
+        if not 'yaml_implicit_resolvers' in cls.__dict__:
+            cls.yaml_implicit_resolvers = cls.yaml_implicit_resolvers.copy()
+
+        for first_letter, mappings in cls.yaml_implicit_resolvers.items():
+            cls.yaml_implicit_resolvers[first_letter] = [(tag, regexp) 
+                                                         for tag, regexp in mappings
+                                                         if tag != tag_to_remove]
 
 
 if __name__ == "__main__":
